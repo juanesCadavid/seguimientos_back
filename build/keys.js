44602fcddd154f7b72ead6b01f2f2e91cfff8040
juanes <juan.epc2000@gmail.com>
2022-02-01 09:46:03 -0500

    1

--- build/keys.js
+++ build/keys.js
@@ -1,4 +1,12 @@
 "use strict";
+// export default{
+//     database: {
+//         host: process.env.DATABASEHOST || '147.182.179.68',
+//         user: process.env.DATABASEUSER || 'admin',
+//         password: process.env.DATABASEPASSWORD || 'Admin.Admin123',
+//         database: process.env.DATABASE_NAME || 'bd_seguimientosprocex'
+//     }
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
